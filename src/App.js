5dcdb342faf377c7e9747012bb5a0e2a77aafb9e
Argian Ardi Prasetya <argianardi1435@gmail.com>
2023-02-17 22:14:45 +0700

    added addProducts and showProducts components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from "react";
+import AddProducts from "./components/AddProducts";
+import ShowProduct from "./components/ShowProduct";
 
 function App() {
   return (
     <div className="container">
-      <h1>Hello world</h1>
+      <div className="columns">
+        <div className="column">
+          <AddProducts />
+        </div>
+        <div className="column">
+          <ShowProduct />
+        </div>
+      </div>
     </div>
   );
 }
