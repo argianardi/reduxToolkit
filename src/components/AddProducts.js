c9961c8124f19a0e1af272d4946776befad1e97e
Argian Ardi Prasetya <argianardi1435@gmail.com>
2023-02-17 23:18:13 +0700

    add update state

--- src/components/AddProducts.js
+++ src/components/AddProducts.js
@@ -1,25 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { update } from "../utils/redux/features/productSlice";
 
 const AddProducts = () => {
+  const [title, setTitle] = useState("");
+  const [price, setPrice] = useState("");
+  const dispatch = useDispatch();
+
+  const updateProduct = (e) => {
+    e.preventDefault();
+    dispatch(update({ title, price }));
+  };
+
   return (
     <div>
-      <from className="box mt-5">
+      <form onSubmit={updateProduct} className="box mt-5">
         <div className="field">
           <label className="label">Title</label>
           <div className="control">
-            <input type="text" placeholder="Title" className="input" />
+            <input
+              type="text"
+              placeholder="Title"
+              className="input"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+            />
           </div>
           <div className="field">
             <label className="label">Price</label>
             <div className="control">
-              <input type="text" placeholder="Price" className="input" />
+              <input
+                type="text"
+                placeholder="Price"
+                className="input"
+                value={price}
+                onChange={(e) => setPrice(e.target.value)}
+              />
             </div>
           </div>
           <div className="field">
             <button className="button is-success">Submit</button>
           </div>
         </div>
-      </from>
+      </form>
     </div>
   );
 };
