3f2dd3e2f4f7718b77bfe5abe9869bfaded0245d
Argian Ardi Prasetya <argianardi1435@gmail.com>
2023-02-20 21:07:12 +0700

    add routes

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./utils/redux/store/store";
-import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "bulma/css/bulma.css";
+import App from "./routes/App";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
